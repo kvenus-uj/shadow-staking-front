b43e5390848192f172fa46b0e93f3ccaeb0a9c03
kvenus-uj <alexstepan0209@gmail.com>
2022-07-02 13:49:10 +0300

    aa

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://knklvl/api",
+  baseURL: "https://knklvl.xyz/api",
   headers: {
     "Content-type": "application/json"
   }
